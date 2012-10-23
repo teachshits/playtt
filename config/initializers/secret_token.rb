6f4afda01be7a0f9c4139499d51232de3e516d08
lessless <me@lessless.pp.ua>
2012-10-23 14:03:44 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playtt::Application.config.secret_token = '9459e69cc9c04842aa22f774dceb38c25dc9252e4c9abe1c30e7db04c1c0eaea3c9b1511138c7b07f43073bb5c55925962fcf398d351418d11a62b40cc9c1b17'
